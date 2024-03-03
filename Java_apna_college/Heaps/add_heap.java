2a5bcb270363c87e7708ac115992b138ec546ca4
n-harshada03 <harshdanavale2003@gmail.com>
2024-03-03 12:40:27 +0530

    heap all functions

--- Java_apna_college/Heaps/add_heap.java
+++ Java_apna_college/Heaps/add_heap.java
@@ -3,28 +3,78 @@ package Java_apna_college.Heaps;
 import java.util.ArrayList;
 
 public class add_heap {
-    public class heap{
-        ArrayList<Integer> arr=new ArrayList<>();
-        int child=arr.size()-1;
-        int parent=(2*child-1)/2;
-        
-        public void add(int data){
+    public class Heap {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        public void add(int data) {
             arr.add(data);
-            while(arr.get(child)<arr.get(parent)){
-                //swap
-                int temp=arr.get(child);
-                arr.set(child,arr.get(parent));
-                arr.set(parent,temp);
+            int child = arr.size() - 1;
+            int parent = (child - 1) / 2;
+
+            while (child > 0 && arr.get(child) < arr.get(parent)) {
+                // Swap
+                int temp = arr.get(child);
+                arr.set(child, arr.get(parent));
+                arr.set(parent, temp);
+
+                child = parent;
+                parent = (child - 1) / 2;
+            }
+        }
+
+        public int peek() {
+            return arr.get(0);
+        }
+
+        private void heapify(int i) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            int minIdx = i;
+
+            if (left < arr.size() && arr.get(minIdx) > arr.get(left)) {
+                minIdx = left;
             }
 
+            if (right < arr.size() && arr.get(minIdx) > arr.get(right)) {
+                minIdx = right;
+            }
+
+            if (minIdx != i) {
+                int temp = arr.get(i);
+                arr.set(i, arr.get(minIdx));
+                arr.set(minIdx, temp);
+                heapify(minIdx);
+            }
         }
-        public int peek(){
-            return arr.get(0);
+
+        public boolean isEmpty() {
+            return arr.size() == 0;
+        }
+
+        public int remove() {
+            if (arr.isEmpty()) {
+                throw new IllegalStateException("Heap is empty");
+            }
+
+            int val = arr.get(0);
+            arr.set(0, arr.get(arr.size() - 1));
+            arr.remove(arr.size() - 1);
+            heapify(0);
+            return val;
         }
     }
 
     public static void main(String[] args) {
-        
+        add_heap h = new add_heap();
+        Heap H = h.new Heap();
+        H.add(3);
+        H.add(4);
+        H.add(1);
+        H.add(5);
+
+        while (!H.isEmpty()) {
+            System.out.println(H.peek());
+            H.remove();
+        }
     }
-    
 }
